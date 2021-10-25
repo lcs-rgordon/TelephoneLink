89711661c3061eb2eb8a305458edd73731f69852
Russell Gordon <rgordon@lcs.on.ca>
2021-10-25 16:56:08 -0400

    Initial Commit

---
+++ TelephoneLink/TelephoneLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelephoneLinkApp.swift
+//  TelephoneLink
+//
+//  Created by Russell Gordon on 2021-10-25.
+//
+
+import SwiftUI
+
+@main
+struct TelephoneLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
